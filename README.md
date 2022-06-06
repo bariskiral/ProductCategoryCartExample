dc8e0c21d9fd6f7265ad133513b1c9f6a01e1940
Barış Kıral <bariskiral07@gmail.com>
2022-06-07 01:30:05 +0300

    update

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ react/react-Redux products and cart test example.
 -react-redux
 -reactstrap
 
+for db:
+json-server --watch db.json
+
 For more: [package.json](https://github.com/bariskiral/ProductCategoryCartExample/blob/master/package.json)
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -4,6 +4,7 @@ import Navi from "../navi/Navi";
 import Dashboard from "./Dashboard";
 import { Routes, Route } from "react-router-dom";
 import CartDetail from "../cart/CartDetail";
+import AddOrUpdateProduct from "../products/AddOrUpdateProduct";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/product" element={<Dashboard />} />
           <Route path="/cart" element={<CartDetail />} />
+          <Route path="/saveproduct/:productId" element={<AddOrUpdateProduct />}></Route>
         </Routes>
       </Container>
     </div>
